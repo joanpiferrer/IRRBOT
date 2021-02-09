162bf4c884835c7c293cfafa3aa64a4518a61a87
Joan Piferrer <joanpiferrer@gmail.com>
2021-02-09 19:42:23 +0000

    backup

--- main_bak.py
+++ main_bak.py
@@ -1,35 +1,23 @@
 from keep_alive import keep_alive
 import discord
 import os
-from discord.utils import get
 
-intents = discord.Intents(members=True)
-client=discord.Client(intents=intents)
 newUserMessage = """
 Benvingut al canal de discord d'Irregulars de PlanB!
 Si no ets soci i necessites preguntar quelcom pots comunicarte mitjançant el canal de text public
 Si ets soci comunica-ho a en Folldelturó i encantats et donarem permisos per que puguis accedir a la resta de canals.
 """
 
+intents = discord.Intents.default()
+intents.members = True
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_member_join(member):
-  await member.create_dm()
-  await member.dm_channel.send(
-      f'Hola {member.name}, ' + newUserMessage
-  )
+    channel = member.guild.get_channel(808624631581376522)
 
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hola'):
-        await message.channel.send('Hola!')
+    await channel.send(f'Hola <@{member.id}>, ' + newUserMessage)
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
